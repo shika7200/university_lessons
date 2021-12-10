2c284bac000027470bdde9a07bbf4132147e6ead
Georgy <shive1997me@gmail.com>
2021-12-10 10:02:12 +0700

    Реализовал методы абстрактного класса.

--- lesson1/lesson1_classes.cpp
+++ lesson1/lesson1_classes.cpp
@@ -3,15 +3,37 @@
  \********************************/
 #include <clocale>
 #include "lesson1_classes.h"
+#include "math.h"
 
 using namespace  std;
 
 class Func
 {
-protected : double x;
+public: double x;
 public: first();
 public: second();
 };
+
+class Z1 : public Func
+{
+
+    double Z1::first() {
+    return ( (pow(x)+ 2*x - 3*x + (x+1) * (sqrt(pow(x) - 9)))/
+            (pow(x)- 2*x -3 + (x+1) * (sqrt(pow(x) - 9 ))))
+                }
+};
+class Z2 : public Func
+        {
+
+            double Z2::second() {
+    return ( sqrt((x+3)/(x-3)))
+// мы же не обязаны реализовывать все методы ? это же не интерфейс я правильно понимаю ?
+    }
+};
+
+
+
+
 
 
 
--- lesson1/main.cpp
+++ lesson1/main.cpp
@@ -1,5 +1,5 @@
 
-/* студбилет 20516 - вариант 16 *\
+/* студ.билет 20516 - вариант 16 *\
  \********************************/
 #include "lesson1_classes.h"
 //#include <conio.h>
@@ -8,16 +8,16 @@
  using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "RUS");
+    setlocale(LC_ALL, "russian");
     cout << "Введите значение x для расчета: ";
-    protected : double x;
-    cin >> x;
+public: double x;
+    cin >> x >> endl();
     cout << " ** Нажмите любую клавишу чтобы расчитать Z1 **" << endl;
     //_getch();// команда задержки  - deprecated in msdos
-
-    cout << "Z1 = " << Z1.first(x) << endl;
+    Z1 obj_z1(); //-- как обьявлять конструктор и деструктор ?компилятор жалуеются на отсутствие скобок, вообще без понятия почему
+    cout << "Z1 = " << Z1.first(x) << endl; // Так ведь можно вызывать конструктор на вывод ?
     cout << "** Нажмите любую клавишу чтобы расчитать Z2 **" << endl;
-    cout << "Z1 = " << Z2.second(x) << endl;
+    cout << "Z1 = " << obj_z2.second(x) << endl;
     cout << "Нажмите любую клавишу чтобы выйти" << endl;
 
     return 0;
