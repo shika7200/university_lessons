6c21c6e27096416dddac0c5e24829bcd86eba086
uguru <shive1997me@gmail.com>
2021-12-22 20:42:47 +0700

    Класс с тремя методами для третьей лабараторки создан.

---
+++ lesson3/cycles.cpp
@@ -0,0 +1,36 @@
+//
+// Created by uguru on 22.12.2021.
+//
+
+#include "cycles.h"
+#include <clocale>
+#include <iostream>
+#include "math.h"
+
+using namespace  std;
+
+
+cycles::cycles(double x)
+{
+    // public: нет смысла дублировать .h
+    Variable = x;
+    cout << "Обьект класса Z1 был создан" << endl;
+}
+cycles::~cycles()
+{
+    cout << "Обьект класса Z1 был уничтожен " << endl;
+}
+
+double cycles::firstSolution()
+{  for
+    return
+}
+
+double cycles::secondSolution() {
+    return
+}
+
+double cycles::thirdSolution() {
+    return
+}
+
---
+++ lesson3/cycles.h
@@ -0,0 +1,16 @@
+//
+// Created by uguru on 22.12.2021.
+//
+
+
+class cycles {
+public:
+    double Variable;
+    Solution(double x);
+    ~Solution();
+    double firstSolution();
+    double secondSolution();
+    double thirdSolution();
+
+};
+
