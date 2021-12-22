dda2174269b4b0fa2074f34273492fc838de9902
shika7200 <shive1997me@gmail.com>
2021-12-22 16:54:33 +0700

    Сделал вторую лабараторную в декларативной парадигме программирования.

--- lesson1/main.cpp
+++ lesson1/main.cpp
@@ -10,12 +10,14 @@
 int main() {
     double x;
     setlocale(LC_ALL, “RUS”);
-    Z1 z1();
-    Z2 z2();
+    Z1 z1(0);
+    Z2 z2(0);
     cout << "Введите значение x для расчета Z1 и Z2: ";
     cin >> x;
-    z1.firstVariable = x;
-    z2.secondVariable = x;
+    Z1 z1(x);
+    Z2 z2(x);
+   // z1.firstVariable = x; слишком сложно
+    //z2.secondVariable = x;
     cout << "Z1 = " << z1.Solution() << endl;
     cout << "Z1 = " << z2.Solution() << endl;
     system("pause")
--- lesson2/main.cpp
+++ lesson2/main.cpp
@@ -1,4 +1,42 @@
 //
-// Created by uguru on 09.12.2021.
+// Created by uguru on 09.12.2021. 6 Вариант Шиве Георгий
 //
+#include <iostream>
+#include <cmath>
+#include <clocale>
 
+
+using namespace std;
+
+float Y(float a, float b, float c, float x) // определение функции
+{
+    return ((4,1*pow(a,3)*x - abs(b))*c )/pow((a+b),2);
+};
+float Z(float a, float b, float c) {
+    return (pow(10, -2)* (a + b )/(pow(a,3)* b))  - exp(a+b);
+
+};
+
+int main() 	// основная программа
+
+{
+    setlocale(LC_ALL, “RUS”);
+    float a, b, c, x, y, z, S;
+    cout<<"Введите значения a, b, c, x: ";
+    cin >> a >> b >> c >> x;
+    if (( a+b == 0) || (pow(a,3)* b))
+        // условие неопределенности
+    {
+        cout << "Возникла неопределенность, вычисления невозможны" << endl;
+    }
+    else
+    {
+        y = Y(a,b,c,x);		// вызов функций
+        z = Z(a,b,c);
+        cout << "Y = "<< y << endl;
+        cout << "Z = "<< z << endl;
+        S = y + z;
+        cout << "S = " << S << endl;
+    }
+    return 0;
+}
