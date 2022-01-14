bca95f82442d1aac3960a4e0848dbb3e0c7dcb57
shika7200 <shive1997me@gmail.com>
2022-01-14 10:52:50 +0300

    что-то поправил

--- lesson5/main.cpp
+++ lesson5/main.cpp
@@ -3,3 +3,28 @@
     <тип>  < ИмяПеременной> [ <колличество элементов > ];
     int A[10]
     char B[23]  float X[15]*/
+    #include <clocale>
+    #include <iostream>
+    #include <cmath>
+    using namespace std;
+
+
+    int main() {
+
+        int A[100]; 
+        int n ;// колличество элементов в  массиве А
+        cout << "Введите n ";
+        cin >> n ;
+        for  ( int i = 0 ; i < n; i++ )
+        {
+            cout << " Введите элемент массива А["<< i << "]= ";
+            cin >> A[i];
+        };
+        // цикл по выводу элементов массива на экран:
+        for ( int i =0; i < n ; i++){
+            cout <<  "A[" << i << "]" << A[i] << endl;
+        }
+       return 0; 
+    }
+
+rand()
