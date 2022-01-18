8842db88d3b66be9a588ee4ef0931f4d387f2a69
shika7200 <shive1997me@gmail.com>
2022-01-18 20:09:27 +0700

    сделал замену чисел больше нуля на нулевые

---
+++ lesson5.2/main.cpp
@@ -0,0 +1,28 @@
+ #include <clocale>
+    #include <iostream>
+    #include <cmath>
+    using namespace std;
+
+
+    int main() {
+
+        int A[100]; 
+        int n ;// колличество элементов в  массиве А
+        cout << "Введите длину массива ";
+        cin >> n ;
+        cout << "Чтобы найти сумму чисел больше указанно, введите это число: ";
+        for  ( int i = 0 ; i < n; i++ )
+        {
+            cout << " Введите элемент массива А["<< i << "]= ";
+            cin >> A[i];
+             if (A[i] > 0) {
+                 A[i] = 0 ; 
+             }
+             
+        };
+        // цикл по выводу элементов массива на экран:
+        for ( int i =0; i < n ; i++){
+            cout <<  "A[" << i << "]" << A[i] << endl;
+        }
+       return 0; 
+    }
