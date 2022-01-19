c1021669614b2cbe04b4d5844956692664544dc0
shika7200 <shive1997me@gmail.com>
2022-01-19 14:53:42 +0500

    добавил 9 лабу

---
+++ lesson9/main.cpp
@@ -0,0 +1,14 @@
+* студбилет 20516 - вариант 16 *\
+ \********************************/
+#include "lesson1_classes.h"
+#include "conio.h"
+#include <iostream>
+#include <clocale>
+ using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
